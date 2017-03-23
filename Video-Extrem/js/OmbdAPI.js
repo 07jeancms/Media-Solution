c7d79545b81aefbf29803c37390017d3f6a1f197
and951 <andresjimqui@hotmail.com>
2017-03-23 11:49:17 -0600

    Agreaga paleta de colores rojos y secciones del carousel
    Agreaga paleta de colores rojos y secciones del carousel

--- Video-Extrem/js/OmbdAPI.js
+++ Video-Extrem/js/OmbdAPI.js
@@ -1,31 +1,7 @@
 var $Form = $('form'), $Container = $('#container');
 $Container.hide();
-<<<<<<< HEAD
-$Form.on('submit', function (p_oEvent) {
-  var sUrl, sMovie, oData;
-  p_oEvent.preventDefault();
-  sMovie = $Form.find('input').val();
-  sUrl = 'https://www.omdbapi.com/?t=' + sMovie + '&type=movie&tomatoes=true';
-  $.ajax(sUrl, {
-    complete: function (p_oXHR, p_sStatus) {
-      oData = $.parseJSON(p_oXHR.responseText);
-      console.log(oData);
-      $Container.find('.title').text(oData.Title);
-      $Container.find('.plot').text(oData.Plot);
-      document.getElementById('moviePoster').src = oData.Poster;
-      $Container.find('.year').text(oData.Year);
-      $Container.show();
-    }
-  });
-});
-=======
-<<<<<<< Updated upstream
-$Form.on('submit', function(p_oEvent){
-   
-=======
 $Form.on('submit', function (p_oEvent) {
     "use strict";
->>>>>>> Stashed changes
     var sUrl, sMovie, oData;
     p_oEvent.preventDefault();
     sMovie = $Form.find('input').val();
@@ -39,13 +15,5 @@ $Form.on('submit', function (p_oEvent) {
             $Container.find('.year').text(oData.Year);
             $Container.show();
         }
-<<<<<<< Updated upstream
-    });    
-});
-
-
-=======
     });
 });
->>>>>>> Stashed changes
->>>>>>> refs/remotes/origin/master
